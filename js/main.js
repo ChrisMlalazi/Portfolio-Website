15e4885f5c2020b7621d53864bc7a6f7e56c3d0f
Christopher Mlalazi <chrismlalazi@gmail.com>
2016-11-04 13:24:06 -0600

    MENU

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-$('.hamburger').on('click', function(e) {
-  // Prevent link from jumping to the top of the page
-  e.preventDefault();
-  // If menu is already showing, slide it up. Otherwise, slide it down.
-  $('.menu').toggleClass('slide-down');
+$("#menu").click(function() {
+  $( ".nav" ).fadeToggle("slow", "linear");
+  $("#menu").addClass("hidden");
+  $("#close").removeClass("hidden");
+});
+$("#close").click(function() {
+  $( ".nav" ).fadeToggle("slow", "linear");
+  $("#menu").removeClass("hidden");
+  $("#close").addClass("hidden"); 
 });
