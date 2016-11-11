0973a3c123ec95effdea052e9fed5da8d9c26e2d
Christopher Mlalazi <chrismlalazi@gmail.com>
2016-11-10 20:35:14 -0600

    JavaScript Hide and Show Projects

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ $('.hamburger').on('click', function(e) {
   // If menu is already showing, slide it up. Otherwise, slide it down.
   $('.menu').toggleClass('slide-down');
 });
+
+//To hide text within each image box
+
+$(".projectText1").hide();
+$(".projectText2").hide();
+
+//To show text within each image box on hover
+
+$(".lightbox li").on("click", function(e){
+	// Prevent link from jumping to the top of the page
+  e.preventDefault();
+  $(".projectText1").show();
+  $(".projectText2").show();
+
+});
+
+
+
